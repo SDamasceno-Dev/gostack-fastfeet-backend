a1f2590cc0dbde79e442e28c80da1268b9dbc1cc
sdamasceno-dev <sdamasceno.dev@gmail.com>
2020-01-31 16:52:47 -0300

    Sequelized configured, database migration created and executed

---
+++ src/database/migrations/20200131184337-create-users.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      // Id User.
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      // NAME User.
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      // EMAIL User.
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      // PASSWORD_HASH Store encrypted password.
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      // Allows to define an administrator user.
+      administrator: {
+        type: Sequelize.BOOLEAN,
+        defaultValues: false,
+        allowNull: false
+      },
+      // Records the creation time
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      // Records the update time
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  }
+};
